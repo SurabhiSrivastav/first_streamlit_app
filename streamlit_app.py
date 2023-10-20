d44a8e084dcea35315e7e9265e4bbd65697cf19d
Surabhi Srivastava <72458190+SurabhiSrivastav@users.noreply.github.com>
2023-10-20 18:20:48 +0530

    Update streamlit_app.py
    added repeatable block in function to get fruityvice choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#     Display Fruityvice api response (previous)
-# streamlit.header("Fruityvice Fruit Advice!")
-#     added text entry box for api input
-# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# streamlit.write('The user entered ', fruit_choice)
-#     import requests needed here
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#  streamlit.text(fruityvice_response.json())  # just writes data to screen
-#       normalizing json form response
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#     presenting data in streamlit screen in form of table
-# streamlit.dataframe(fruityvice_normalized)
+
+# create the repeatable code block(called function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -46,9 +40,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruitvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
  
 except URLError as e:
   streamlit.error()
